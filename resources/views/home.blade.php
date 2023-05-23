ffaf65af54f7e3e3f55ba94ca2cdedf679086862
LeoneCarmelo <carmeloleone@outlook.it>
2023-05-23 18:05:28 +0200

    Fix shape cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,12 @@
 @extends('layout.app')
 
 @section('content') 
-<div class="container my-5">
+<div class="container py-5">
     <div class="row row-cols-1 row-cols-md-3 row-cols-lg-6 g-3">
         @foreach ($comics as $comic)
         <div class="col">
-            <div class="card">
-                <img src='{{$comic["thumb"]}}' alt="" class="card-img-top rounded">
+            <div class="card_thumb">
+                <img src='{{$comic["thumb"]}}' alt="" class="card-img-top ">
             </div>
             <span class="text-white">{{ $comic["series"] }}</span>
         </div>
