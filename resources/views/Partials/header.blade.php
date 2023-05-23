4b173e8f34155098a4bcfe010d5638f82d83fa74
LeoneCarmelo <carmeloleone@outlook.it>
2023-05-23 16:57:55 +0200

    Add header

--- resources/views/Partials/header.blade.php
+++ resources/views/Partials/header.blade.php
@@ -1,3 +1,25 @@
 <header>
-    Header
+        <div class="blue_strip">
+            <div class="container d-flex justify-content-end py-2">
+                <span class="mx-3 text-white">Dc Power Visa</span>
+                <span class="text-white">Additional Dc Sites</span> 
+            </div>
+        </div>
+        <div class="container d-flex justify-content-center my-3">
+            <div class="logo">
+                <img  class="img-fluid" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            </div>
+            <ul class="d-flex gap-3 justify-content-center py-4">
+                <li>CHARACTERS</li>
+                <li>COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
+        </div>
 </header>
